a241a99634f960ea739480f1e7fb5955dcec0dcc
David George <off.davidgeorge@gmail.com>
2022-01-29 15:53:17 +0530

    verse also done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 /* eslint-disable default-case */
 import { useState, useEffect } from "react";
 import CustomSelect from "./components/CustomSelect";
-import { Loader, Dimmer, Segment, Image } from 'semantic-ui-react'
 
 function App() {
   const [selectedBook, setSelectedBook] = useState(null);
   const [selectedChapter, setSelectedChapter] = useState(null);
   const [htmlText, setHtmlText] = useState(null);
+  const [IsChapterMenuOpen, setIsChapterMenuOpen] = useState(false);
 
   useEffect(() => {
     let html = '';
 
-    fetch('https://labs.bible.org/api/?passage=john+1&type=json&formatting=full')
+    fetch('https://labs.bible.org/api/?passage=john+1&type=json&formatting=para')
       .then((res) => {return res.json()})
-      .then((json) => {json.map((obj) => (html += obj.text))})
+      .then((json) => {json.map((obj) => (obj.text.includes('<p') === true ? html += obj.text.slice(0, 20) + `<sup><b>${obj.verse}</b></sup> ` + obj.text.slice(20) : html += `<sup><b>${obj.verse}</b></sup> ${obj.text}`))})
       .then(() => setHtmlText(html))
       .then(() => document.querySelector('#chapterTitle').textContent = 'John 1')
+      .catch((error) => {document.getElementById('errorText').textContent = 'Sorry, server is down. Try again after a few minutes.'})
   }, []);
   
 
@@ -23,11 +24,12 @@ function App() {
     let html = '';
     setHtmlText('');
 
-    fetch(`https://labs.bible.org/api/?passage=${selectedBook}+${selectedChapter}&type=json&formatting=full`)
+    fetch(`https://labs.bible.org/api/?passage=${selectedBook}+${selectedChapter}&type=json&formatting=para`)
       .then((res) => {return res.json()})
-      .then((json) => {json.map((obj) => (html += obj.text))})
+      .then((json) => {json.map((obj) => (obj.text.includes('<p') === true ? html += obj.text.slice(0, 20) + `<sup><b>${obj.verse}</b></sup> ` + obj.text.slice(20) : html += `<sup><b>${obj.verse}</b></sup> ${obj.text}`))})
       .then(() => setHtmlText(html))
       .then(() => document.querySelector('#chapterTitle').textContent = selectedBook + ' ' + selectedChapter)
+      .then(() => {document.getElementById('errorText').textContent = ''})
   }, [selectedBook, selectedChapter]);
   
 
@@ -70,32 +72,25 @@ function App() {
 
   let handleOnChangeBookOptions = (obj) => {
     setSelectedBook(obj.label);
+    setIsChapterMenuOpen(true);
   }
 
   let handleOnChangeChapterOptions = (obj) => {
     setSelectedChapter(obj.value);
-  }
-
-  if (!htmlText) {
-    <Segment>
-      <Dimmer active>
-        <Loader>Loading</Loader>
-      </Dimmer>
-
-      <Image src='https://react.semantic-ui.com/images/wireframe/short-paragraph.png' />
-    </Segment>
+    setIsChapterMenuOpen(false);
   }
 
   return (
     <div className="App">
+      <p className="text-3xl text-red-600 flex justify-center items-center mt-2 mb-5" id="errorText"></p>
 
       <div className="flex flex-row justify-center items-center mb-6">
         <CustomSelect options={bookOptions} defaultValue={{ label: 'Select Book', value: 'selectBook' }} styles={{ width: '200px' }} onChangeFunc={handleOnChangeBookOptions}/>
-        {selectedBook && <CustomSelect options={returnObjectOptions(chapterOptionsFinder(selectedBook))} defaultValue={{ label: 1, value: 1 }} styles={{ width: '100px', marginLeft: '15px' }} onChangeFunc={handleOnChangeChapterOptions} />}
+        {selectedBook && <CustomSelect options={returnObjectOptions(chapterOptionsFinder(selectedBook))} defaultValue={{ label: 1, value: 1 }} styles={{ width: '100px', marginLeft: '15px' }} onChangeFunc={handleOnChangeChapterOptions} menuOnOrOff={IsChapterMenuOpen} />}
       </div>
 
       <p id="chapterTitle" className="flex justify-center items-center text-3xl font-serif mb-3"></p>
-      {htmlText && <main dangerouslySetInnerHTML={{ __html: htmlText }} className="flex flex-col items-center"></main>}
+      {htmlText && <main dangerouslySetInnerHTML={{ __html: htmlText }} className="flex flex-col items-center text-lg"></main>}
     </div>
   );
 }
--- src/components/CustomSelect.jsx
+++ src/components/CustomSelect.jsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import Select from 'react-select';
 
-const CustomSelect = ({ options, defaultValue, styles, onChangeFunc }) => {
-  // let options = [
-  //   {label: 'Matthew', value: 'matthew'},
-  //   {label: 'Mark', value: 'mark'},
-  //   {label: 'Luke', value: 'luke'},
-  //   {label: 'John', value: 'john'}
-  // ]
-
+const CustomSelect = ({ options, defaultValue, styles, onChangeFunc, menuOnOrOff }) => {
   return (
     <div style={styles}>
-      <Select options={options} defaultValue={defaultValue} onChange={onChangeFunc} />
+      <Select options={options} defaultValue={defaultValue} onChange={onChangeFunc} menuIsOpen={menuOnOrOff} />
     </div>
   )
 };
