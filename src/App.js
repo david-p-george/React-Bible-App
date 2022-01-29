5c8a1f1ba3d24b982e3dee4a3c2d1529077169c8
David George <off.davidgeorge@gmail.com>
2022-01-29 12:43:08 +0530

    hmmmm

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 /* eslint-disable default-case */
 import { useState, useEffect } from "react";
 import CustomSelect from "./components/CustomSelect";
+import { Loader, Dimmer, Segment, Image } from 'semantic-ui-react'
 
 function App() {
   const [selectedBook, setSelectedBook] = useState(null);
@@ -14,18 +15,19 @@ function App() {
       .then((res) => {return res.json()})
       .then((json) => {json.map((obj) => (html += obj.text))})
       .then(() => setHtmlText(html))
+      .then(() => document.querySelector('#chapterTitle').textContent = 'John 1')
   }, []);
   
 
   useEffect(() => {
     let html = '';
-    // document.querySelector('main').innerHTML = '';
     setHtmlText('');
 
     fetch(`https://labs.bible.org/api/?passage=${selectedBook}+${selectedChapter}&type=json&formatting=full`)
       .then((res) => {return res.json()})
       .then((json) => {json.map((obj) => (html += obj.text))})
       .then(() => setHtmlText(html))
+      .then(() => document.querySelector('#chapterTitle').textContent = selectedBook + ' ' + selectedChapter)
   }, [selectedBook, selectedChapter]);
   
 
@@ -74,15 +76,25 @@ function App() {
     setSelectedChapter(obj.value);
   }
 
+  if (!htmlText) {
+    <Segment>
+      <Dimmer active>
+        <Loader>Loading</Loader>
+      </Dimmer>
+
+      <Image src='https://react.semantic-ui.com/images/wireframe/short-paragraph.png' />
+    </Segment>
+  }
+
   return (
     <div className="App">
-      <h1 className="text-3xl">Hello!</h1>
 
-      <div className="flex flex-row">
-        <CustomSelect options={bookOptions} defaultValue={{label: 'Matthew', value: 'matthew'}} styles={{ width: '300px' }} onChangeFunc={handleOnChangeBookOptions}/>
+      <div className="flex flex-row justify-center items-center mb-6">
+        <CustomSelect options={bookOptions} defaultValue={{ label: 'Select Book', value: 'selectBook' }} styles={{ width: '200px' }} onChangeFunc={handleOnChangeBookOptions}/>
         {selectedBook && <CustomSelect options={returnObjectOptions(chapterOptionsFinder(selectedBook))} defaultValue={{ label: 1, value: 1 }} styles={{ width: '100px', marginLeft: '15px' }} onChangeFunc={handleOnChangeChapterOptions} />}
       </div>
 
+      <p id="chapterTitle" className="flex justify-center items-center text-3xl font-serif mb-3"></p>
       {htmlText && <main dangerouslySetInnerHTML={{ __html: htmlText }} className="flex flex-col items-center"></main>}
     </div>
   );
